ade55d17acb56371a73e00e9301f69644dbff6fd
George Gelbakhiani <108350829+gelbakhiani@users.noreply.github.com>
2023-01-17 21:46:19 -0500

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,31 @@
 from machinetranslation import translator
+
 from flask import Flask, render_template, request
 import json
 
-app = Flask("Web Translator")
+app = Flask("WebTranslator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    # print("english to fr" + textToTranslate)
+    frenchText=translator.englishToFrench(textToTranslate)
+    return "Translated text to French: " + frenchText
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    englishToFrench = translator.frenchToEnglish(textToTranslate)
+    return "Translated text to English: " + englishToFrench
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+
+    return render_template('index.html')
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
